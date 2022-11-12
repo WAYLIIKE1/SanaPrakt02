327b4bbeb595e1dd897c66c7b8e1d89d4aa05296
Walenty Sawicki <sana22_vsb@student.ztu.edu.ua>
2022-11-12 15:33:31 +0100

    Changed first 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Make");
+Console.WriteLine("Some");
+Console.WriteLine("Noise:)");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
